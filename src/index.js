c7ef82edff02037bb2677b41dbd6fdc5cef71a7d
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-15 19:53:09 -0300

    fix: resolve bugs in route index file

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const clientRoutes = require('./routes/clientRoutes'); // Rutas de cliente
+const productRoutes = require('./routes/productRoutes'); // Rutas de producto
+const orderRoutes = require('./routes/orderRoutes'); // Rutas de pedido
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/restapishop', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Configuración de rutas
+app.use('/clients', clientRoutes); // Prefijo de ruta para clientes
+app.use('/products', productRoutes); // Prefijo de ruta para productos
+app.use('/orders', orderRoutes); // Prefijo de ruta para pedidos
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
