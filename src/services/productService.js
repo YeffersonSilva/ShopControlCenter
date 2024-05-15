2fd7b3fd2ee24e9a801ab5a0e2382ae90c259082
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-15 19:52:31 -0300

    feat: create services file for centralized business logic

---
+++ src/services/productService.js
@@ -0,0 +1,8 @@
+/*const Product = require('../models/Product');
+
+exports.addProduct = async (data) => {
+    const product = new Product(data);
+    return await product.save();
+};
+
+// Otros métodos de servicio según sea necesario
