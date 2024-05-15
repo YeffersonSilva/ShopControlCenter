608bc1603f836571b1b8b1a00cc713b23a1b2297
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-15 20:04:42 -0300

    fix: correct routing errors

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const newOrderController = require('../controllers/order/newOrderController'); // Make sure this path is correct
-const deleteOrderController = require('../controllers/order/deleterOrderController');
+const newOrderController = require('../controllers/order/newOrdersController'); // Make sure this path is correct
+const deleteOrderController = require('../controllers/order/deleteOrderController');
 const getOrderController = require('../controllers/order/getOrderController');
 const getOrdersController = require('../controllers/order/getOrdersController');
 const updateOrderController = require('../controllers/order/updateOrderController');
