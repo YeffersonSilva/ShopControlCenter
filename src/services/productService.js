85a5bc6a89e47283131cd68ec17093b1dbf5142d
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-15 19:55:38 -0300

    feat: enhance image upload functionality

--- src/services/productService.js
+++
@@ -1,8 +0,0 @@
-/*const Product = require('../models/Product');
-
-exports.addProduct = async (data) => {
-    const product = new Product(data);
-    return await product.save();
-};
-
-// Otros métodos de servicio según sea necesario
