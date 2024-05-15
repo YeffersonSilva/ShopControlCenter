5f39847edf500c52c351f4108ae699fcf7139ddd
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-15 19:52:23 -0300

    feat: add order routes for CRUD operations

---
+++ src/routes/productRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+
+const addProductController = require('../controllers/product/addProductController')
+const uploadImageController = require('../controllers/product/uploadImageController');
+const getProductsController = require('../controllers/product/getProductsController');
+const getProductController = require('../controllers/product/getProductController');
+
+const updateProductController = require('../controllers/product/updateProductController');
+const deleteProductController = require('../controllers/product/deleteProductController');
+
+
+router.post('/', uploadImageController.uploadImage, addProductController.addProduct); // Usar el middleware de carga de imagen
+router.get('/', getProductsController.getProducts);
+router.get('/:id', getProductController.getProduct);
+router.put('/:id',uploadImageController.uploadImage, updateProductController.udpatProduct);
+
+router.delete('/:id', deleteProductController.deleteProduct);
+
+module.exports = router;
