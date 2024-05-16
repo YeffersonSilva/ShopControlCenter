c2ac66560fba978481de0d6d3ce5f70f75339ade
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-16 14:24:39 -0300

    fix: resolve error in product image upload

--- back-end/src/controllers/product/uploadImageController.js
+++ back-end/src/controllers/product/uploadImageController.js
@@ -1,32 +1,24 @@
 const multer = require('multer');
 const shortid = require('shortid');
+const path = require('path');
 
-const configMulter = {
-    storage: multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, __dirname + '/../../uploads');
-        },
-        filename: (req, file, cb) => {
-            const extension = file.mimetype.split('/')[1];
-            cb(null, `${shortid.generate()}.${extension}`);
-        }
-    }),
-    fileFilter(req, file, cb) {
-        if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-            cb(null, true);
-        } else {
-            cb(new Error('Invalid format'));
-        }
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../../uploads'));
+    },
+    filename: (req, file, cb) => {
+        const extension = file.mimetype.split('/')[1];
+        cb(null, `${shortid.generate()}.${extension}`);
     }
-};
+});
 
-const upload = multer(configMulter).single('image');
+const upload = multer({ storage }).single('image');
 
 exports.uploadImage = (req, res, next) => {
     upload(req, res, function (error) {
         if (error) {
-            return res.status(400).json({ message: error.message });
+            res.json({ message: error });
         }
-        next();
+        return next();
     });
 };
