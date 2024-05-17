9ee3e9978b6ff9ccdc077259bd189d32f0df6c57
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-17 16:21:23 -0300

    feat: update DetallesPedido component to display correct order details
    - Updated DetallesPedido component to use the correct properties from the order object
    - Fixed key value for mapping order items to prevent duplication
    - Updated variable names to match the updated API response structure

--- front-end/src/componentes/pedidos/DetallesPedido.js
+++ front-end/src/componentes/pedidos/DetallesPedido.js
@@ -1,30 +1,28 @@
 import React from 'react';
 
 function DetallesPedido({pedido}) {
-
     const {cliente} = pedido;
 
     return(
         <li className="pedido">
             <div className="info-pedido">
-                <p className="id">ID: 0192019201291201</p>
-                <p className="nombre">Cliente: {cliente.nombre} {cliente.apellido} </p>
+                <p className="id">ID: {pedido._id}</p>
+                <p className="nombre">Cliente: {cliente.name} {cliente.surname}</p>
 
                 <div className="articulos-pedido">
-                    <p className="productos">Artículos Pedido: </p>
+                    <p className="productos">Artículos Pedido:</p>
                     <ul>
-                        {pedido.pedido.map(articulos => (
-                            <li key={pedido._id+articulos.producto._id}>
-                                <p>{articulos.producto.nombre} </p>
-                                <p>Precio: ${articulos.producto.precio} </p>
-                                <p>Cantidad: {articulos.cantidad}</p>
+                        {pedido.order.map(articulos => (
+                            <li key={pedido._id+articulos.product._id}>
+                                <p>{articulos.product.name} </p>
+                                <p>Precio: ${articulos.product.price} </p>
+                                <p>Cantidad: {articulos.quantity}</p>
                             </li>
                         ))}
                     </ul>
                 </div>
 
                 <p className="total">Total: ${pedido.total} </p>
-
             </div>
             <div className="acciones">
                 <button type="button" className="btn btn-rojo btn-eliminar">
--- front-end/src/componentes/pedidos/FormBuscarProducto.js
+++ front-end/src/componentes/pedidos/FormBuscarProducto.js
@@ -1,29 +1,27 @@
 import React from 'react';
 
 function FormBuscarProducto(props) {
-    return(
-            <form
-                onSubmit={props.buscarProducto}
-            >
-                <legend>Busca un Producto y agrega una cantidad</legend>
+    return (
+        <form onSubmit={props.buscarProducto}>
+            <legend>Busca un Producto y agrega una cantidad</legend>
 
-                <div className="campo">
-                    <label>Productos:</label>
-                    <input 
-                        type="text" 
-                        placeholder="Nombre Productos" 
-                        name="productos" 
-                        onChange={props.leerDatosBusqueda}
-                    />
-                </div>
-
+            <div className="campo">
+                <label>Productos:</label>
                 <input
-                    type="submit"
-                    className="btn btn-azul btn-block"
-                    value="Buscar Producto"
+                    type="text"
+                    placeholder="Nombre Productos"
+                    name="productos"
+                    onChange={props.leerDatosBusqueda}
                 />
+            </div>
 
-            </form>
-    )
+            <input
+                type="submit"
+                className="btn btn-azul btn-block"
+                value="Buscar Producto"
+            />
+        </form>
+    );
 }
+
 export default FormBuscarProducto;
